42cecde3c4ea164f580f0a7f736549f084276a20
Raffaele Sena <rsena@adobe.com>
2012-06-13 15:25:34 -0700

    adding copyright notice

--- dynash.py
+++ dynash.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
+# Copyright (c) 2012 Raffaele Sena http://www.aromatic.org/
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 
-# Make all string literals unicode
+#
+# A simple shell to interact with DynamoDB
+#
+
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 try:
     import cmd2 as cmd
-    print "using cmd2"
 except ImportError:
     import cmd
-    print "using cmd"
 
 import boto
 import json
